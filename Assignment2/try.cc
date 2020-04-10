38dc8c680cf01edf773dfe0ef4775464572bd8d0
SDTheSlayer <dalal.shivang@gmail.com>
2020-04-10 22:25:49 +0530

    tcp &udp

--- Assignment2/try.cc
+++ Assignment2/try.cc
@@ -1,6 +1,8 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-
+#include <map>
+#include <string>
 #include "ns3/netanim-module.h"
+#include "ns3/flow-monitor-module.h"
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/csma-module.h"
@@ -20,23 +22,22 @@ main (int argc, char *argv[])
     CommandLine cmd;
     cmd.Parse (argc, argv);
 
-    Time::SetResolution (Time::NS);
-    LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-    LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
-
     std::string rate_hr = "80Mbps";
 	std::string lat_hr = "20ms";
 	std::string rate_rr = "30Mbps";
 	std::string lat_rr = "100ms";
+    double simulation_time = 10.0;
+    uint32_t packet_size_tcp = 1000;
+    uint32_t packet_size_udp = 50;  
 
 	uint packetSize = 1.2*1024;		//1.2KB
-	uint qsize_hr = (100000*20)/packetSize;
-	uint qsize_rr = (10000*100)/packetSize;
+	uint qsize_hr = (10000*20)/packetSize;
+	uint qsize_rr = (1000*100)/packetSize;
     std::string q_hr_str= std::to_string(qsize_hr) + "p";
     std::string q_rr_str= std::to_string(qsize_rr) + "p";
 
 
-    NS_LOG_INFO ("Create nodes.");
+   std::cout<<"Create nodes.";
     NodeContainer all_nodes;
     all_nodes.Create(6);
 
@@ -49,7 +50,7 @@ main (int argc, char *argv[])
     InternetStackHelper stack;
     stack.Install (all_nodes);
 
-    NS_LOG_INFO ("Create channels.");
+    std::cout<<"Create channels.";
     PointToPointHelper p2p_hr, p2p_rr;
     p2p_hr.SetDeviceAttribute("DataRate", StringValue(rate_hr));
 	p2p_hr.SetChannelAttribute("Delay", StringValue(lat_hr));
@@ -66,7 +67,7 @@ main (int argc, char *argv[])
     NetDeviceContainer dl_dr = p2p_rr.Install (rl_rr);
 
 
-    NS_LOG_INFO ("Assign IP Addresses.");
+    std::cout<<"Assign IP Addresses.";
     Ipv4AddressHelper ipv4;
     ipv4.SetBase ("172.16.1.0", "255.255.255.0");
     Ipv4InterfaceContainer il_i0 = ipv4.Assign (dl_d0);
@@ -88,29 +89,70 @@ main (int argc, char *argv[])
     uint16_t port = 2;
     OnOffHelper onoff ("ns3::UdpSocketFactory", InetSocketAddress (ir_i3.GetAddress (1), port));
     onoff.SetConstantRate (DataRate ("10kbps"));
-    onoff.SetAttribute ("PacketSize", UintegerValue (50));
+    onoff.SetAttribute ("PacketSize", UintegerValue (packet_size_udp));
 
     ApplicationContainer apps = onoff.Install (all_nodes.Get (1));
     apps.Start (Seconds (1.0));
-    apps.Stop (Seconds (10.0));
+    apps.Stop (Seconds (simulation_time));
 
     PacketSinkHelper sink ("ns3::UdpSocketFactory",
                             Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
     apps = sink.Install (all_nodes.Get (4));
     apps.Start (Seconds (1.0));
-    apps.Stop (Seconds (10.0));
-    // BulkSendHelper ftp("ns3::TcpSocketFactory", InetSocketAddress (ir_i4.GetAddress (1), port));
-    // ftp.SetAttribute ("SendSize", UintegerValue (100));
+    apps.Stop (Seconds (simulation_time));
+
+
+  
+    BulkSendHelper ftp("ns3::TcpSocketFactory", InetSocketAddress (ir_i4.GetAddress (1), port));
+    ftp.SetAttribute ("SendSize", UintegerValue (packet_size_tcp));
+    ftp.SetAttribute ("MaxBytes", UintegerValue (0));
+    ApplicationContainer ftpApp = ftp.Install (all_nodes.Get (0));
+    ftpApp.Start (Seconds (1.0));
+    ftpApp.Stop (Seconds (simulation_time)); 
+    
+
+    PacketSinkHelper sink3 ("ns3::TcpSocketFactory",
+                            Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
+    ftpApp = sink3.Install (all_nodes.Get (5));
+    ftpApp.Start (Seconds (1.0));
+    ftpApp.Stop (Seconds (simulation_time));
+    Ptr<FlowMonitor> flowmon;
+	FlowMonitorHelper flowmonHelper;
+	flowmon = flowmonHelper.InstallAll();
+
+    flowmon->SetAttribute("DelayBinWidth", DoubleValue(0.001));
+    flowmon->SetAttribute("JitterBinWidth", DoubleValue(0.001));
+    flowmon->SetAttribute("PacketSizeBinWidth", DoubleValue(20)); 
 
     AnimationInterface anim ("animation.xml");
     anim.SetConstantPosition(all_nodes.Get(0),10.0,10.0);
-    anim.SetConstantPosition(all_nodes.Get(1),10.0,30.0);
-    anim.SetConstantPosition(all_nodes.Get(2),20.0,20.0);
-    anim.SetConstantPosition(all_nodes.Get(3),30.0,20.0);
-    anim.SetConstantPosition(all_nodes.Get(4),40.0,30.0);
-    anim.SetConstantPosition(all_nodes.Get(5),40.0,10.0);
-    NS_LOG_INFO ("Running Simulation.");
-    Simulator::Run ();
+    anim.SetConstantPosition(all_nodes.Get(1),10.0,60.0);
+    anim.SetConstantPosition(all_nodes.Get(2),40.0,40.0);
+    anim.SetConstantPosition(all_nodes.Get(3),60.0,40.0);
+    anim.SetConstantPosition(all_nodes.Get(4),80.0,60.0);
+    anim.SetConstantPosition(all_nodes.Get(5),80.0,10.0);
+
+
+	Simulator::Stop(Seconds(simulation_time+2));
+
+	Simulator::Run();
+	flowmon->CheckForLostPackets();
+
+
+
+
+
+    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmonHelper.GetClassifier());
+
+	std::map<FlowId, FlowMonitor::FlowStats> stats = flowmon->GetFlowStats();
+
+	for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin(); i != stats.end(); ++i) {
+		Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (i->first);
+		std::cout << "TcpReno Flow " << i->first  << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")\n";
+		std::cout  << "Net Packet Lost: " << i->second.lostPackets << "\n";
+	}
+
     Simulator::Destroy ();
     NS_LOG_INFO ("Done :)");
+    
 }
