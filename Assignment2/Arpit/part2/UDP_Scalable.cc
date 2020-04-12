5f89bcbf5b1fd19e12e60c9fdd34c45b92ba6150
Arpit Gupta <arpit170101012@iitg.ac.in>
2020-04-12 15:29:28 +0530

    UDP_scalable together

--- Assignment2/Arpit/part2/UDP_Scalable.cc
+++ Assignment2/Arpit/part2/UDP_Scalable.cc
@@ -53,16 +53,19 @@ void ThroughputMonitor (FlowMonitorHelper* flowmonHelper, Ptr<FlowMonitor> flowM
 int 
 main (int argc, char *argv[])
 {
-    Gnuplot2dDataset dataset, delay_dataset;
-    dataset.SetTitle ("UDP_throughput");
-	dataset.SetStyle (Gnuplot2dDataset::LINES_POINTS);
-    delay_dataset.SetTitle ("UDP_delay");
-	delay_dataset.SetStyle (Gnuplot2dDataset::LINES_POINTS);
+    Gnuplot2dDataset UTdataset, UDdataset, STdataset, SDdataset;
+    // dataset.SetTitle ("UDP_throughput");
+	UTdataset.SetStyle (Gnuplot2dDataset::LINES_POINTS);
+    // delay_dataset.SetTitle ("UDP_delay");
+	UDdataset.SetStyle (Gnuplot2dDataset::LINES_POINTS);
+    SDdataset.SetStyle (Gnuplot2dDataset::LINES_POINTS);
+    STdataset.SetStyle (Gnuplot2dDataset::LINES_POINTS);
+    
 
 
     CommandLine cmd;
     cmd.Parse (argc, argv);
-    // Config::SetDefault ("ns3::TcpL4Protocol::SocketType", TypeIdValue (TypeId::LookupByName ("ns3::TcpScalable")));
+    Config::SetDefault ("ns3::TcpL4Protocol::SocketType", TypeIdValue (TypeId::LookupByName ("ns3::TcpScalable")));
     throughfout.open("throughput_udp_alone.csv", std::ios::out);
     delayfout.open("delay_udp_alone.csv", std::ios::out);
 
@@ -70,9 +73,9 @@ main (int argc, char *argv[])
 	std::string lat_hr = "20ms";
 	std::string rate_rr = "30Mbps";
 	std::string lat_rr = "100ms";
-    double simulation_time = 5.0;
+    double simulation_time = 10.0;
     // uint32_t packet_size_tcp = 1000;
-    // uint32_t packet_size_udp = 50;  
+    // uint32_t packet_size = 50;  
 
 	// uint packetSize = 1.2*1024;		//1.2KB
 	uint qsize_hr = 100000*20;
@@ -89,8 +92,9 @@ main (int argc, char *argv[])
     // Sending data from h1 to h3
     // UDP server
     uint16_t port = 2;
-    for (uint32_t packet_size_udp = 512; packet_size_udp < 5000; packet_size_udp+=64)
+    for (uint32_t packet_size = 512; packet_size < 5000; packet_size+=64)
     {
+        Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue(packet_size));
         NodeContainer all_nodes;
         all_nodes.Create(6);
 
@@ -146,7 +150,7 @@ main (int argc, char *argv[])
         /* code */
         OnOffHelper onoff ("ns3::UdpSocketFactory", InetSocketAddress (ir_i3.GetAddress (1), port));    //h3
         onoff.SetConstantRate (DataRate ("1000kbps"));   // why 100kbps
-        onoff.SetAttribute ("PacketSize", UintegerValue (packet_size_udp));
+        onoff.SetAttribute ("PacketSize", UintegerValue (packet_size));
 
         ApplicationContainer client_apps = onoff.Install (all_nodes.Get (0));  //h1-->server node
 
@@ -161,6 +165,23 @@ main (int argc, char *argv[])
 
         client_apps.Start (Seconds (1.0));
         client_apps.Stop (Seconds (simulation_time));
+
+        //--------------------------------------------TCP
+        BulkSendHelper ftp("ns3::TcpSocketFactory", InetSocketAddress (ir_i4.GetAddress (1), port));    // h4 is sink
+        ftp.SetAttribute ("SendSize", UintegerValue (packet_size));
+        ftp.SetAttribute ("MaxBytes", UintegerValue (0));
+        ApplicationContainer ftpApp = ftp.Install (all_nodes.Get (1));
+        ftpApp.Start (Seconds (1.0));
+        ftpApp.Stop (Seconds (simulation_time)); 
+        
+
+        PacketSinkHelper sink3 ("ns3::TcpSocketFactory",
+                                Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
+        ApplicationContainer sink_ftpApp = sink3.Install (all_nodes.Get (5));
+        sink_ftpApp.Start (Seconds (1.0));
+        sink_ftpApp.Stop (Seconds (simulation_time));
+        //------------------------------------------------
+
         //----------------------------------------------------------
         NS_LOG_INFO("Run Simulation");
         Ptr<FlowMonitor> flowmon;
@@ -170,11 +191,11 @@ main (int argc, char *argv[])
         flowmon->SetAttribute("DelayBinWidth", DoubleValue(0.001));
         flowmon->SetAttribute("JitterBinWidth", DoubleValue(0.001));
         flowmon->SetAttribute("PacketSizeBinWidth", DoubleValue(20)); 
-        double throughput=0;
+        double Uthroughput=0, Sthroughput=0;
         int count=0;
-        double delay=0;
-        int total_packets=0;
-        Simulator::Stop(Seconds(6.0));
+        double Udelay=0, Sdelay=0;
+        int Utotal_packets=0, Stotal_packets=0;
+        Simulator::Stop(Seconds(12.0));
         Simulator::Run();
         
 
@@ -192,30 +213,51 @@ main (int argc, char *argv[])
         { 
             count++;
             Ipv4FlowClassifier::FiveTuple fiveTuple = classing->FindFlow (stats->first);
+            if(fiveTuple.sourceAddress=="172.16.1.2" && fiveTuple.destinationAddress=="172.16.3.2")
+            {
+                std::cout<<"UDP found"<<std::endl;
+                Utotal_packets = stats->second.txPackets;
+                Uthroughput += stats->second.rxBytes * 8.0 / (stats->second.timeLastRxPacket.GetSeconds() - stats->second.timeFirstTxPacket.GetSeconds())/1024/1024;            // std::cout<<"Throughput: " << throughput << " Mbps"<<std::endl;
+                Udelay = stats->second.delaySum.GetSeconds();
+            }
+            else if(fiveTuple.sourceAddress=="172.16.2.2" && fiveTuple.destinationAddress=="172.16.4.2")
+            {
+                // if(count==1)
+                // {
+                std::cout<<"TCP found"<<std::endl;
+                Stotal_packets = stats->second.txPackets;
+                Sthroughput += stats->second.rxBytes * 8.0 / (stats->second.timeLastRxPacket.GetSeconds() - stats->second.timeFirstTxPacket.GetSeconds())/1024/1024;            // std::cout<<"Throughput: " << throughput << " Mbps"<<std::endl;
+                Sdelay = stats->second.delaySum.GetSeconds();
+                // }
+            }
+            
             std::cout<<"Flow ID     : " << stats->first <<" ; "<< fiveTuple.sourceAddress <<" -----> "<<fiveTuple.destinationAddress<<std::endl;
-            // std::cout<<"Tx Packets = " << stats->second.txPackets<<std::endl;
-            total_packets = stats->second.txPackets;
+            std::cout<<"Tx Packets = " << stats->second.txPackets<<std::endl;
+            // total_packets = stats->second.txPackets;
             // std::cout<<"Rx Packets = " << stats->second.rxPackets<<std::endl;
             // std::cout<<"Duration    : "<<stats->second.timeLastRxPacket.GetSeconds()-stats->second.timeFirstTxPacket.GetSeconds()<<std::endl;
             // std::cout<<"Last Received Packet  : "<< stats->second.timeLastRxPacket.GetSeconds()<<" Seconds"<<std::endl;
-            throughput += stats->second.rxBytes * 8.0 / (stats->second.timeLastRxPacket.GetSeconds() - stats->second.timeFirstTxPacket.GetSeconds())/1024/1024;            // std::cout<<"Throughput: " << throughput << " Mbps"<<std::endl;
+            // throughput += stats->second.rxBytes * 8.0 / (stats->second.timeLastRxPacket.GetSeconds() - stats->second.timeFirstTxPacket.GetSeconds())/1024/1024;            // std::cout<<"Throughput: " << throughput << " Mbps"<<std::endl;
             // std::cout<< "Net Packet Lost: " << stats->second.lostPackets << "\n";
-            // std::cout<<"Delay Sum :"<<stats->second.delaySum<<std::endl;;
-            delay = stats->second.delaySum.GetSeconds();
+            std::cout<<"Delay Sum :"<<stats->second.delaySum<<std::endl;
+            // delay = stats->second.delaySum.GetSeconds();
             // std::cout<<stats->second.rxBytes<<" "<<stats->second.timeLastRxPacket.GetSeconds()-stats->second.timeFirstTxPacket.GetSeconds()<<'\n';
-            // std::cout<<"---------------------------------------------------------------------------"<<std::endl;
+            std::cout<<"---------------------------------------------------------------------------"<<std::endl;
             // throughfout<<(double)stats->first<<", "<<throughput<<", ";
             // delayfout<<(double)stats->first<<", "<<stats->second.delaySum.GetMilliSeconds()-delays[stats->first]<<", ";
             // delays[stats->first]=stats-/>second.delaySum.GetMilliSeconds();
         }
-        std::cout<<packet_size_udp<<" "<<throughput/count<<" "<<count<<std::endl;
-        std::cout<<packet_size_udp<<" "<<delay/total_packets<<" "<<count<<std::endl;
-        dataset.Add(packet_size_udp, throughput/count);
-        delay_dataset.Add(packet_size_udp, delay/total_packets);
-        // throughfout<<'\n';
-        // delayfout<<'\n';
-        // flowmon->SerializeToXmlFile("lab-5.flowmon", true, true);
-        // Simulator::Schedule(MilliSeconds(100),&ThroughputMonitor, &flowmonHelper, flowmon);  
+        std::cout<<packet_size<<" "<<Uthroughput<<" "<<count<<std::endl;
+        std::cout<<packet_size<<" "<<Udelay/Utotal_packets<<" "<<count<<std::endl;
+        std::cout<<packet_size<<" "<<Sthroughput<<" "<<count<<std::endl;
+        std::cout<<packet_size<<" "<<Sdelay/Stotal_packets<<" "<<count<<std::endl;
+
+        // UTdataset, UDdataset, STdataset, SDdataset;
+        UTdataset.Add(packet_size, Uthroughput);
+        UDdataset.Add(packet_size, Udelay/Utotal_packets);
+        STdataset.Add(packet_size, Sthroughput);
+        SDdataset.Add(packet_size, Sdelay/Stotal_packets);
+
         Simulator::Destroy(); 
         //-------------------------------------- 
         
@@ -224,15 +266,15 @@ main (int argc, char *argv[])
     
 
     //Initialize Plot file names  
-    std :: string fileNameWithNoExtension = "UDP_throughput";
+    std :: string fileNameWithNoExtension = "USthroughput";
     std :: string graphicsFileName        = fileNameWithNoExtension + ".png";
     std :: string plotFileName            = fileNameWithNoExtension + ".plt";
-    std :: string plotTitle               = "udp throughput vs packet size";
+    std :: string plotTitle               = "udp and scalable throughput vs packet size";
 
-    std :: string fileNameWithNoExtension_delay = "UDP_delay";
+    std :: string fileNameWithNoExtension_delay = "USdelay";
     std :: string graphicsFileName_delay        = fileNameWithNoExtension_delay + ".png";
     std :: string plotFileName_delay            = fileNameWithNoExtension_delay + ".plt";
-    std :: string plotTitle_delay               = "udp delay vs packet size";
+    std :: string plotTitle_delay               = "udp and scalable delay vs packet size";
 
    
     // Instantiate the plot and set its title.
@@ -252,13 +294,15 @@ main (int argc, char *argv[])
     
     //Set the x value ranges for each plots
     // plot.AppendExtra ("set xrange [0:800]");
-    plot.AppendExtra ("set yrange [0:+2.0]");
-    plot_delay.AppendExtra ("set yrange [0.140:+0.143]");
+    plot.AppendExtra ("set yrange [0:+3.0]");
+    plot_delay.AppendExtra ("set yrange [0.135:+0.143]");
     
     
     // Add the dataset to the plot.
-    plot.AddDataset (dataset);
-    plot_delay.AddDataset(delay_dataset);
+    plot.AddDataset (UTdataset);
+    plot.AddDataset(STdataset);
+    plot_delay.AddDataset(UDdataset);
+    plot_delay.AddDataset(SDdataset);
     
     // Open the plot file for fairness
     std::ofstream plotFile (plotFileName.c_str());
